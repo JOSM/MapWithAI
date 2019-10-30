a181a4d4466fe400cf0cc8e91894e435a45ed425
Taylor Smock <taylor.smock@kaart.com>
2019-10-30 16:16:53 -0600

    Revert "Fix direction of command undo/redo"
    This reverts commit 6ddc12ce9d88a6bf71b4c82f0a94679f21db92d9.

--- src/main/java/org/openstreetmap/josm/plugins/mapwithai/backend/MergeDuplicateWaysAction.java
+++ src/main/java/org/openstreetmap/josm/plugins/mapwithai/backend/MergeDuplicateWaysAction.java
@@ -15,6 +15,7 @@ import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.plugins.mapwithai.MapWithAIPlugin;
 import org.openstreetmap.josm.plugins.mapwithai.commands.MergeDuplicateWays;
+import org.openstreetmap.josm.tools.Logging;
 import org.openstreetmap.josm.tools.Shortcut;
 
 /**
@@ -37,16 +38,21 @@ public class MergeDuplicateWaysAction extends JosmAction {
     public void actionPerformed(ActionEvent e) {
         final List<Way> ways = new ArrayList<>(MainApplication.getLayerManager().getActiveDataSet().getSelectedWays());
         Command command = null;
-        if (ways.size() == 2) {
-            command = new MergeDuplicateWays(ways.get(0), ways.get(1));
-        } else if (ways.size() == 1) {
-            command = new MergeDuplicateWays(ways.get(0));
-        } else if (ways.isEmpty()) {
-            command = new MergeDuplicateWays(MainApplication.getLayerManager().getActiveDataSet());
-        }
-        if (command != null) {
-            UndoRedoHandler.getInstance().add(command);
-        }
+        int i = 0;
+        do {
+            if (ways.size() == 2) {
+                command = new MergeDuplicateWays(ways.get(0), ways.get(1));
+            } else if (ways.size() == 1) {
+                command = new MergeDuplicateWays(ways.get(0));
+            } else if (ways.isEmpty()) {
+                command = new MergeDuplicateWays(MainApplication.getLayerManager().getActiveDataSet());
+            }
+            if (command != null) {
+                UndoRedoHandler.getInstance().add(command);
+                i++;
+                Logging.error(Integer.toString(i));
+            }
+        } while (command != null && i < 10);
     }
 
 }
