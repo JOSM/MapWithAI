69f4252085d3da7361236968ecc4399ef827fb0e
Taylor Smock <taylor.smock@kaart.com>
2020-04-20 11:17:31 -0600

    JOSM r16321 removed `Territories.getDataSet`
    Switch to `Territories.getOriginalDataSet`
    
    Signed-off-by: Taylor Smock <taylor.smock@kaart.com>

--- src/main/java/org/openstreetmap/josm/plugins/mapwithai/backend/MapWithAIAvailability.java
+++ src/main/java/org/openstreetmap/josm/plugins/mapwithai/backend/MapWithAIAvailability.java
@@ -92,7 +92,7 @@ public final class MapWithAIAvailability extends DataAvailability {
     private static Map<String, Map<String, Boolean>> parseForCountries(JsonArray countries) {
         final Map<String, Map<String, Boolean>> returnCountries = new TreeMap<>();
         Territories.initialize();
-        final DataSet territories = Territories.getDataSet();
+        final DataSet territories = Territories.getOriginalDataSet();
         for (int i = 0; i < countries.size(); i++) {
             final JsonObject country = countries.getJsonObject(i).getJsonObject("properties");
             for (String countryName : cornerCaseNames(country.getString("Country"))) {
