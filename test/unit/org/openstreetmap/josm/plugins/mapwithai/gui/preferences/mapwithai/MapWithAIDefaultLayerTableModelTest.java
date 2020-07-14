3b94fe1150ac979cc3d65a86a552ff0904a2da52
Taylor Smock <taylor.smock@kaart.com>
2020-07-14 10:18:57 -0600

    Fix an NPE in a test
    Signed-off-by: Taylor Smock <taylor.smock@kaart.com>

--- test/unit/org/openstreetmap/josm/plugins/mapwithai/gui/preferences/mapwithai/MapWithAIDefaultLayerTableModelTest.java
+++ test/unit/org/openstreetmap/josm/plugins/mapwithai/gui/preferences/mapwithai/MapWithAIDefaultLayerTableModelTest.java
@@ -20,7 +20,7 @@ import org.openstreetmap.josm.testutils.JOSMTestRules;
 
 public class MapWithAIDefaultLayerTableModelTest {
     @RegisterExtension
-    JOSMTestRules rule = new MapWithAITestRules().wiremock().sources();
+    JOSMTestRules rule = new MapWithAITestRules().wiremock().sources().projection();
 
     @Test
     public void testGetRow() {
