98031cc0f3a6579a822a5c3b2a240454f589033b
Taylor Smock <tsmock@fb.com>
2022-01-05 13:14:33 -0700

    Remove Esri detection for too many objects
    This is due to issues with streams and parsing JSON.
    
    Signed-off-by: Taylor Smock <tsmock@fb.com>

--- test/unit/org/openstreetmap/josm/plugins/mapwithai/backend/BoundingBoxMapWithAIDownloaderTest.java
+++ test/unit/org/openstreetmap/josm/plugins/mapwithai/backend/BoundingBoxMapWithAIDownloaderTest.java
@@ -4,19 +4,11 @@ package org.openstreetmap.josm.plugins.mapwithai.backend;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import javax.json.Json;
-import javax.json.JsonObjectBuilder;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.openstreetmap.josm.data.Bounds;
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.gui.progress.NullProgressMonitor;
@@ -28,7 +20,6 @@ import org.openstreetmap.josm.testutils.JOSMTestRules;
 import org.openstreetmap.josm.testutils.annotations.BasicPreferences;
 import org.openstreetmap.josm.testutils.annotations.BasicWiremock;
 import org.openstreetmap.josm.testutils.annotations.HTTP;
-import org.openstreetmap.josm.tools.Logging;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.admin.model.GetServeEventsResult;
@@ -53,38 +44,6 @@ class BoundingBoxMapWithAIDownloaderTest {
 
     @BasicWiremock
     public WireMockServer wireMockServer;
-
-    @ParameterizedTest
-    @ValueSource(strings = { "text/json", "application/json", "application/geo+json" })
-    void testEsriExceededTransferLimit(String responseType) {
-        final Bounds downloadBounds = new Bounds(10, 10, 20, 20);
-        final MapWithAIInfo info = new MapWithAIInfo("testEsriExceededTransferLimit",
-                this.wireMockServer.baseUrl() + "/esriExceededLimit");
-        final BoundingBoxMapWithAIDownloader boundingBoxMapWithAIDownloader = new BoundingBoxMapWithAIDownloader(
-                downloadBounds, info, false);
-        final JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
-        objectBuilder.add("type", "FeatureCollection");
-        objectBuilder.add("properties", Json.createObjectBuilder().add("exceededTransferLimit", true));
-        objectBuilder.add("features", Json.createArrayBuilder());
-        wireMockServer.stubFor(WireMock
-                .get(boundingBoxMapWithAIDownloader
-                        .getRequestForBbox(downloadBounds.getMinLon(), downloadBounds.getMinLat(),
-                                downloadBounds.getMinLon(), downloadBounds.getMinLat())
-                        .replace(wireMockServer.baseUrl(), ""))
-                .willReturn(WireMock.aResponse().withHeader("Content-Type", responseType)
-                        .withBody(objectBuilder.build().toString())));
-
-        Logging.clearLastErrorAndWarnings();
-        final DataSet ds = assertDoesNotThrow(
-                () -> boundingBoxMapWithAIDownloader.parseOsm(NullProgressMonitor.INSTANCE));
-        List<String> errors = new ArrayList<>(Logging.getLastErrorAndWarnings());
-        // Needed to avoid CI failures
-        errors.removeIf(str -> str.contains("Failed to persist preferences"));
-        assertEquals(1, errors.size(),
-                "We weren't handling transfer limit issues. Are we now?\n" + String.join("\n", errors));
-        assertTrue(errors.get(0).contains("Could not fully download"));
-        assertTrue(ds.isEmpty());
-    }
 
     @Test
     void testThirdPartyConflation() {
